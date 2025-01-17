e828ab83b4a877f928a4ae4aeac8773f9444c573
jshoonn <shoon3012@gmail.com>
2025-01-17 17:17:14 +0900

    minor change in variable.py

--- progressive/variable.py
+++ progressive/variable.py
@@ -7,33 +7,45 @@ class Variable:
         self.expr = expr
 
     def __add__(self, other):
-        return Variable(Addition(self, other))
+        return Variable(self.loop, Addition(self, other))
 
     def __iadd__(self, other):
         self.expr = Addition(self, other)
         return self    
+    
+    def __radd__(self, other):
+        return Variable(self.loop, Addition(other, self))
 
     def __mul__(self, other):
-        return Variable(Multiplication(self, other))
+        return Variable(self.loop, Multiplication(self, other))
     
     def __imul__(self, other):
         self.expr = Multiplication(self, other)
         return self
     
+    def __rmul__(self, other):
+        return Variable(self.loop, Multiplication(other, self))
+    
     def __sub__(self, other):
-        return Variable(Subtraction(self, other))
+        return Variable(self.loop, Subtraction(self, other))
     
     def __isub__(self, other):
         self.expr = Subtraction(self, other)
         return self    
     
+    def __rsub__(self, other):
+        return Variable(self.loop, Subtraction(other, self))
+    
     def __truediv__(self, other):
-        return Variable(Division(self, other))
+        return Variable(self.loop, Division(self, other))
                 
     def __itruediv__(self, other):
         self.expr = Division(self, other)
         return self
     
+    def __rtruediv__(self, other):
+        return Variable(self.loop, Division(other, self)) 
+    
     def __pow__(self, other):
         if isinstance(other, int) and other > 0:
-            return Variable(PowerN(self, other))
+            return Variable(self.loop, PowerN(self, other))
