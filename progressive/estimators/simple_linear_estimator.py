a3348726a61091d3283f2a8b08eb8a72b4a35a53
Jaemin Jo <jmjo@skku.edu>
2025-01-17 09:54:21 +0900

    added linear estimator

---
+++ progressive/estimators/simple_linear_estimator.py
@@ -0,0 +1,61 @@
+import time
+
+def linear_regression(history):
+    x_values = [x for x, y in history]
+    y_values = [y for x, y in history]
+    
+    x_mean = sum(x_values) / len(x_values)
+    y_mean = sum(y_values) / len(y_values)
+    
+    numerator = sum((x - x_mean) * (y - y_mean) for x, y in history)
+    denominator = sum((x - x_mean) ** 2 for x in x_values)
+    m = numerator / denominator
+
+    c = y_mean - m * x_mean
+
+    return m, c
+
+class SimpleLinearEstimator:
+    def __init__(self, init_iter_count = 1, min_iter_count = 1):
+        self.init_iter_count = init_iter_count
+        self.min_iter_count = min_iter_count
+        
+        self.history = [] # (iter, time) tuples
+        self.iter = None
+        
+    def estimate_next(self, tick):
+        """ returns the number of iterations to run in ``tick`` seconds"""
+        iter = self.init_iter_count
+
+        if len(self.history) > 2:
+            history = self.history[-10:]
+            m, c = linear_regression(history)
+
+            iter = (1 - c) / m
+
+        iter = max(self.min_iter_count, int(iter))
+        self.iter = iter    
+            
+        return iter
+        
+    def start(self):
+        self.start_time = time.time()
+
+    def end(self):
+        self.history.append((self.iter, time.time() - self.start_time))
+        self.iter = None
+
+            
+    
+        
+
+        
+
+        
+
+    
+
+
+    
+
+
